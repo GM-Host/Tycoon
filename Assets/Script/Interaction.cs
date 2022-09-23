461edd14b8edd0005076229e135cd0c4284d8aab
Jangmanbo <wkddpdnjs99@gmail.com>
2022-09-24 00:44:41 +0900

    add : book object
    - 발라드의 일지 오브젝트 추가
    - 클릭->발라드의 일지 클로즈업

--- Assets/Script/Interaction.cs
+++ Assets/Script/Interaction.cs
@@ -8,13 +8,13 @@ public class Interaction : MonoBehaviour
     [SerializeField]
     private Animator stampAnimator;
     [SerializeField]
-    private GameObject closeUp;
+    private GameObject closeUp; // 클로즈업 상태일 때 배경
     [SerializeField]
-    private GameObject closeUpIdentity, closeUpTierSeal;
+    private GameObject closeUpIdentity, closeUpTierSeal, closeUpBook;   // 클로즈업 신분증, 증표, 발라드의 일지
     [SerializeField]
-    private GameObject toad;
+    private GameObject toad;    // 도장 가진 두꺼비 오브젝트
     [SerializeField]
-    private Sprite toadImage1, toadImage2;
+    private Sprite toadImage1, toadImage2;  // 도장 품은 두꺼비, 도장 뱉은 두꺼비 이미지
 
     private bool stampActive;
 
@@ -29,15 +29,20 @@ public class Interaction : MonoBehaviour
             closeUpIdentity.SetActive(false);
             GameObject.Find("Canvas").transform.Find("Identity(Clone)").GetComponent<SpawnObject>().ReleaseCloseUp();
         }
-        else    // 증표 클로즈업 상태
+        else if (closeUpTierSeal.activeSelf == true)   // 증표 클로즈업 상태
         {
             // 증표 클로즈업 해제
             closeUpTierSeal.SetActive(false);
             GameObject.Find("Canvas").transform.Find("TierSeal(Clone)").GetComponent<SpawnObject>().ReleaseCloseUp();
         }
-
+        else    // 발라드의 일지 클로즈업 상태
+        {
+            // 발라드의 일지 클로즈업 해제
+            closeUpBook.SetActive(false);
+        }
     }
 
+    // 두꺼비 클릭
     public void ClickToad()
     {
         if (stampActive)
@@ -53,6 +58,15 @@ public class Interaction : MonoBehaviour
         stampActive = !stampActive;
     }
 
+    // 발라드의 일지 클릭
+    public void ClickBook()
+    {
+        Debug.Log("발라드의 일지 클릭");
+        closeUp.SetActive(true);
+        closeUpBook.SetActive(true);
+    }
+
+    // 벨 클릭
     public void ClickBell()
     {
         Debug.Log("벨 클릭");
