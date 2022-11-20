69a85246c24ebaaefcd0d121d67ba11b8d7f2a36
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2022-11-20 11:21:03 +0900

    update : sprite and inventory
    요리사 이미지 수정
    인벤토리 및 플레이버 텍스트 창 생성

--- Assets/Script/Cook/CookUI.cs
+++ Assets/Script/Cook/CookUI.cs
@@ -29,14 +29,14 @@ public class CookUI : MonoBehaviour
 
     [Header("Cooking Buttons")]
     private bool actionBtn_NPC = false;
-    public GameObject boilingBtn, steamingBtn, boildownBtn, roastingBtn, fryingBtn, stirfryingBtn, mixingBtn;
+    public GameObject boilingBtn, steamingBtn, boildownBtn, roastingBtn, fryingBtn, stirfryingBtn, mixingBtn, inventory, flavorTxt;
     public void ClickedNPC()
     {
         // 요리 버튼 ON/OFF
         actionBtn_NPC = !actionBtn_NPC;
         boilingBtn.SetActive(actionBtn_NPC); steamingBtn.SetActive(actionBtn_NPC); boildownBtn.SetActive(actionBtn_NPC); 
         roastingBtn.SetActive(actionBtn_NPC); fryingBtn.SetActive(actionBtn_NPC); stirfryingBtn.SetActive(actionBtn_NPC);
-        mixingBtn.SetActive(actionBtn_NPC);
+        mixingBtn.SetActive(actionBtn_NPC); inventory.SetActive(actionBtn_NPC); flavorTxt.SetActive(actionBtn_NPC);
         
     }
 
