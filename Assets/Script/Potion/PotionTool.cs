59b9cc0fc6b350f2472b6a17d4b24e7c34548e34
Jangmanbo <wkddpdnjs99@gmail.com>
2022-12-22 17:15:49 +0900

    refactor : drop item on tool
    도구로 아이템 드래그
    -> PotionTool에서 DropItem 클래스 instance의 Drop 함수를 호출

--- Assets/Script/Potion/PotionTool.cs
+++ Assets/Script/Potion/PotionTool.cs
@@ -2,15 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
+// J : 포션 제조 씬에서 도구(갈기, 끓이기, 빠히)에 부착하는 스크립트
 public class PotionTool : MonoBehaviour, IDropHandler
 {
-    private float gravity = 980f;
-
-    [SerializeField] private Vector2 track;
-
-    [SerializeField] private GameObject DropItem;
+    [SerializeField] private Vector2 moveRange; // J : (아이템이 떨어지기 시작하는 위치, 멈추는 위치) <-y축 기준
 
     // 이 슬롯에 무언가 마우스 드롭
     public void OnDrop(PointerEventData eventData)
@@ -18,30 +14,7 @@ public class PotionTool : MonoBehaviour, IDropHandler
         if (DragSlot.instance.dragSlot != null)
         {
             Debug.Log(DragSlot.instance.dragSlot.item.name + " 드롭!");
-            DropItem.GetComponent<Image>().sprite = DragSlot.instance.dragSlot.item.itemImage;
-            DragSlot.instance.dragSlot.SetSlotCount(-1);    // J : 재료 1개 소비
-
-            StartCoroutine(MoveCoroutine());
-        }
-    }
-
-    private IEnumerator MoveCoroutine()
-    {
-        float mVelocity = 500f;
-        DropItem.transform.position = new Vector2(transform.position.x, track.x);
-        Debug.Log(transform.position.x);
-        Debug.Log(DropItem.transform.position.x);
-        Vector3 pos = DropItem.transform.position;
-
-        while (pos.y > track.y)
-        {
-            pos = DropItem.transform.position;
-            mVelocity += gravity * Time.deltaTime;
-
-            pos.y -= mVelocity * Time.deltaTime;
-            DropItem.transform.position = pos;
-
-            yield return null;
+            DropItem.instance.Drop(transform.position.x, moveRange);
         }
     }
 }
