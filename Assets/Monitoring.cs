78ee63c992c3b4b8539c08a3d06acfca0324ca35
daysandmoons1 <daysandmoons1@gmail.com>
2023-01-10 14:59:24 +0900

    장비 수정 리소스 및 오브젝트 배치

---
+++ Assets/Monitoring.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Repair
+{
+    public class Monitoring : MonoBehaviour
+    {
+        // 마우스 호버링 시 팝업 활성화
+    }
+}
