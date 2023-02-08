296947a36e07f2b895bc461f15302e9e4d3fef43
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-02-09 00:45:47 +0900

    Update : result comes after cutscenes
    컷씬 뒤에 결과창 띄우기

--- Assets/Script/Cook/ResultUI.cs
+++ Assets/Script/Cook/ResultUI.cs
@@ -11,6 +11,7 @@ public class ResultUI : MonoBehaviour
     [SerializeField] private GameObject resultUI;
     [SerializeField] private Image food;
     [SerializeField] private Text resultText;
+    private string str_Food, foodTxt;
     public void ShowResult(string result, string [] processes, int count, string str_food, string foodtxt)
     {
         if(result == "Success")
@@ -18,10 +19,9 @@ public class ResultUI : MonoBehaviour
                 // 컷씬
                 resultScene.SetActive(true);
                 StartCoroutine(CookProcess(processes, count));
-                // 결과창
-                resultUI.SetActive(true);
-                food.sprite = Resources.Load("Cook/food/"+str_food, typeof(Sprite)) as Sprite;
-                resultText.text = foodtxt;
+                // 결과창 이용
+                str_Food = str_food;
+                foodTxt = foodtxt;
             }
 
         else
@@ -35,5 +35,14 @@ public class ResultUI : MonoBehaviour
             cutScene.sprite = Resources.Load("Cook/result/" + processes[i]+"1", typeof(Sprite)) as Sprite;
             yield return new WaitForSeconds(3f);
         }
+        ShowFood();
+    }
+    private void ShowFood()
+    {
+        resultScene.SetActive(false);
+        // 결과창
+        resultUI.SetActive(true);
+        food.sprite = Resources.Load("Cook/food/"+str_Food, typeof(Sprite)) as Sprite;
+        resultText.text = foodTxt;
     }
 }
