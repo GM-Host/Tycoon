1d6300d109a08cf6cd949d868214052ee92b9992
daysandmoons1 <daysandmoons1@gmail.com>
2023-02-26 15:21:04 +0900

    [장비 수리] 달구기

---
+++ Assets/Script/Repair/Furnace/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Repair
+{
+    class Weapon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public static GameObject itemBeingDragged;
+        Vector3 startPosition;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            itemBeingDragged = gameObject;
+            startPosition = transform.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = Input.mousePosition;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            itemBeingDragged = null;
+            transform.position = startPosition;
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            if(collision.gameObject.name == "Begin")
+                Debug.Log("ㅇㅇ");
+        }
+    }
+}
