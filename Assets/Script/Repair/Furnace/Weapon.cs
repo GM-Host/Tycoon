821879c0b7e1ecc4caffc6b48ebf66087e670507
daysandmoons1 <daysandmoons1@gmail.com>
2023-02-26 15:49:25 +0900

    [장비 수리] 호버링 작업 중

--- Assets/Script/Repair/Furnace/Weapon.cs
+++ Assets/Script/Repair/Furnace/Weapon.cs
@@ -1,35 +1,68 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 namespace Repair
 {
     class Weapon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
+        // 드래그
         public static GameObject itemBeingDragged;
-        Vector3 startPosition;
+        private Vector3 startPosition;
+
+        // 불꽃 탐지용 레이
+        private float distance;
+        private RaycastHit2D rayHit;
+        private Ray2D ray;
+
+        // 호버링
+        private bool hovering = false;
+        private Image gaugeBar;
+        private float fLimitTime;
+        private string strType = "";
+
+        void FixedUpdate()
+        {
+            if(hovering)
+            {
+
+            }
+        }
 
         public void OnBeginDrag(PointerEventData eventData)
         {
             itemBeingDragged = gameObject;
             startPosition = transform.position;
+
+            ray = new Ray2D();
+            ray.origin = startPosition;
+            ray.direction = transform.forward;
+
+            rayHit = Physics2D.Raycast(transform.position, -Vector2.up);
         }
 
         public void OnDrag(PointerEventData eventData)
         {
             transform.position = Input.mousePosition;
+
+            if((rayHit = Physics2D.Raycast(transform.position, -Vector2.up)))
+            {
+                string tstrFireName = rayHit.transform.name;
+                switch(tstrFireName)
+                {
+                    case "Begin": break;
+                    case "Brave": break;
+                    case "Bless": break;
+                    case "Clear": break;
+                }
+            }
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
             itemBeingDragged = null;
             transform.position = startPosition;
-        }
-
-        private void OnCollisionStay2D(Collision2D collision)
-        {
-            if(collision.gameObject.name == "Begin")
-                Debug.Log("ㅇㅇ");
         }
     }
 }
