319f0a05a63df15784664e7ff201adedb9294d93
daysandmoons1 <daysandmoons1@gmail.com>
2023-03-01 13:58:57 +0900

    [장비 수리] 망치질 구현 중

---
+++ Assets/Script/Repair/Hammering/Hammering.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Hammering
+{
+    class Hammering : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] KeyboardObjs;
+        [SerializeField] private Sprite[] keyImgs;
+
+        // 3~6개 중 랜덤으로 활성화 될 오브젝트 고르기
+        
+
+        // 랜덤으로 키보드 설정
+
+    }
+}
